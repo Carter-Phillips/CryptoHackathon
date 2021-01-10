2f3fe1880adca0b0bcfe5e1ff94153df597b5ca3
Carter Phillips <carter.joel.phillips@gmail.com>
2021-01-09 22:29:33 -0800

    added price checker from binance API

--- Scraper/Analyzer.py
+++ Scraper/Analyzer.py
@@ -13,7 +13,7 @@ def analyze(input):
     redis_client = redis.from_url(
         'redis://:p05ca3a5a99da6bbd8bd06cd80912e4f89710bcc2149d75a64c645485ae85d9eb@ec2-174-129-249-71.compute-1.amazonaws.com:9029')
 
-
+    print(response)
 
     # YES RAGHAV I YOINKED YOUR CODE GET HECKED ON
     coinResults = []
@@ -28,6 +28,7 @@ def analyze(input):
 
             coinResults.append(
                 CoinSentiment(coin.decode("utf-8") if type(coin) != str else coin, entity.sentiment.score))
+
 
     return coinResults
 

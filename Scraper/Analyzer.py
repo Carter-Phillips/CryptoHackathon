deb0d2fc6c48dd84009c12ee18a48fb1c170a520
Carter Phillips <carter.joel.phillips@gmail.com>
2021-01-09 19:30:39 -0800

    Updated analyzer to return coin entity sentiment

--- Scraper/Analyzer.py
+++ Scraper/Analyzer.py
@@ -3,13 +3,15 @@ import redis
 import json
 from google.cloud import language_v1
 
+
 def analyze(input):
     """Run a sentiment analysis request on text within a passed filename."""
     client = language_v1.LanguageServiceClient()
 
     document = language_v1.Document(content=input, type_=language_v1.Document.Type.PLAIN_TEXT)
     response = client.analyze_entity_sentiment(request={'document': document})
-    client = redis.from_url('redis://:p05ca3a5a99da6bbd8bd06cd80912e4f89710bcc2149d75a64c645485ae85d9eb@ec2-174-129-249-71.compute-1.amazonaws.com:9029')
+    client = redis.from_url(
+        'redis://:p05ca3a5a99da6bbd8bd06cd80912e4f89710bcc2149d75a64c645485ae85d9eb@ec2-174-129-249-71.compute-1.amazonaws.com:9029')
 
     coinSentiment = []
 
@@ -22,19 +24,10 @@ def analyze(input):
             # decoding is required if it is pulled from the db
             # since
 
+            coinSentiment.append(
+                CoinSentiment(coin.decode("utf-8") if type(coin) != str else coin, entity.sentiment.score))
 
-            coinSentiment.append(CoinSentiment(coin.decode("utf-8") if type(coin) != str else coin, entity.sentiment.score))
-
-
-    for coin in coinSentiment:
-        print(coin.coin, coin.sentiment)
     return coinSentiment
-
-    # Print the results
-
-
-    # annotations has: .document_sentiment.score, .document_sentiment.magnitude, .sentences, .sentences.score
-
 
 class CoinSentiment:
     def __init__(self, coin, sentiment):

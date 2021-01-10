954d43b2c00900c776fb001de4caa8bfd7aea113
Carter Phillips <carter.joel.phillips@gmail.com>
2021-01-09 20:11:45 -0800

    Naming clarity

--- Scraper/Analyzer.py
+++ Scraper/Analyzer.py
@@ -10,23 +10,23 @@ def analyze(input):
 
     document = language_v1.Document(content=input, type_=language_v1.Document.Type.PLAIN_TEXT)
     response = client.analyze_entity_sentiment(request={'document': document})
-    client = redis.from_url(
+    redis_client = redis.from_url(
         'redis://:p05ca3a5a99da6bbd8bd06cd80912e4f89710bcc2149d75a64c645485ae85d9eb@ec2-174-129-249-71.compute-1.amazonaws.com:9029')
 
 
 
     # YES RAGHAV I YOINKED YOUR CODE GET HECKED ON
-    coinSentiment = []
+    coinResults = []
     for entity in response.entities:
-        if client.hexists('cryptos', entity.name):
-            coin = client.hget('cryptos', entity.name)
+        if redis_client.hexists('cryptos', entity.name):
+            coin = redis_client.hget('cryptos', entity.name)
             if coin.upper() == coin:  # means it is a symbol not a name
                 coin = entity.name
 
             # decoding is required if it is pulled from the db
             # since
 
-            coinSentiment.append(
+            coinResults.append(
                 CoinSentiment(coin.decode("utf-8") if type(coin) != str else coin, entity.sentiment.score))
 
     return coinSentiment
